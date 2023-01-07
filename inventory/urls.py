2af7ee55c6bf958742e9f81675210bf10dd83718
JPCLima <jpclima93@gmail.com>
2023-01-07 23:20:27 +0100

    Update views & urls

--- inventory/urls.py
+++ inventory/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
+    path('recipes/', views.RecipesView.as_view(), name='recipes'),
+    path('inventory/', views.InventoryView.as_view(), name='inventory'),
+    path('purchase/', views.PurchaseView.as_view(), name='purchase'),
+    path('report/', views.ReportView.as_view(), name='report'),
 ]
--- inventory/views.py
+++ inventory/views.py
@@ -1,5 +1,24 @@
-from django.views.generic import TemplateView
+from django.views.generic import ListView, TemplateView
+
+from .models import Ingredient
 
 
 class HomeView(TemplateView):
     template_name = 'inventory/home.html'
+
+
+class InventoryView(ListView):
+    template_name = 'inventory/inventory.html'
+    model = Ingredient
+
+
+class RecipesView(TemplateView):
+    template_name = 'inventory/recipes.html'
+
+
+class PurchaseView(TemplateView):
+    template_name = 'inventory/purchase.html'
+
+
+class ReportView(TemplateView):
+    template_name = 'inventory/report.html'

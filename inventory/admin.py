4d0ba3337fc4bd4e60c370cf022f2c8c4a6aebd1
JPCLima <jpclima93@gmail.com>
2023-01-12 00:15:37 +0100

    Create new db tabels

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,10 +1,29 @@
 from django.contrib import admin
 
-from .models import Ingredient
+from .models import Ingredient, MenuItem, Purchase, RecipeManager
 
 
 class IngridientsAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'quantity', 'unit', 'price_unit')
 
 
+class MenuItemAdmin(admin.ModelAdmin):
+    list_display = ('title', 'price')
+
+
+class RecipeManagerAdmin(admin.ModelAdmin):
+    """ list_display = ('menu_items', 'ingredient', 'quantity') """
+    list_display = ("menu_items", "get_ingredients", "quantity")
+
+    def get_ingredients(self, obj):
+        return [ingredient.name for ingredient in obj.ingredient.all()]
+
+
+class PurchaseAdmin(admin.ModelAdmin):
+    list_display = ('menu_item', 'timestamp')
+
+
 admin.site.register(Ingredient, IngridientsAdmin)
+admin.site.register(MenuItem, MenuItemAdmin)
+admin.site.register(Purchase, PurchaseAdmin)
+admin.site.register(RecipeManager, RecipeManagerAdmin)
--- inventory/models.py
+++ inventory/models.py
@@ -11,3 +11,56 @@ class Ingredient(models.Model):
 
     def get_absolute_url(self):
         return "/ingredients"
+
+    def __str__(self):
+        return f"""
+        name: {self.name};
+        quantity: {self.quantity};
+        unit: {self.unit};
+        price_unit: {self.price_unit}
+        """
+
+
+class MenuItem(models.Model):
+    """Menu Item represents a entry menu in the restaurant"""
+    title = models.CharField(max_length=200, unique=True)
+    price = models.FloatField(default=0.00)
+
+    def get_absolute_url(self):
+        return "/menu"
+
+    def __str__(self):
+        return f"""{self.title} | {self.price}"""
+
+
+class RecipeManager(models.Model):
+    """Represents a ingredients needed for a recipe"""
+    menu_items = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    ingredient = models.ManyToManyField(Ingredient)
+    quantity = models.FloatField(default=0.0)
+
+    def get_absolute_url(self):
+        return "/menu"
+
+    def __str__(self):
+        return f"""{self.menu_items} | {self.quantity}"""
+
+
+class Purchase(models.Model):
+    """Represent the purchase of the menu"""
+    menu_item = models.ForeignKey(RecipeManager, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_created=True)
+
+    def get_absolute_url(self):
+        return "/purchases"
+
+    def save(self, *args, **kwargs):
+        self.menu_item.quantity -= 1
+        if self.menu_item.quantity > 1:
+            self.menu_item.save()
+            super().save(*args, **kwargs)
+        else:
+            return False
+
+    def __str__(self):
+        return f"""{self.menu_item} | {self.timestamp}"""
